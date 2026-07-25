fbfdc89fbce6d34e8d7879b38f9433adc9db7c1c
Yogendra Dhami <yogendradhami@Yogendras-MacBook-Air.local>
2026-07-26 09:27:56 +0930

    Fix HEIC conversion in gallery upload form

--- gallery/forms.py
+++ gallery/forms.py
@@ -1,9 +1,46 @@
 from django import forms
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from io import BytesIO
+
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+register_heif_opener()
 
 from .models import GalleryItem
 
 
+class HEICImageField(forms.ImageField):
+
+    def clean(self, data, initial=None):
+        file = super().clean(data, initial)
+
+        if file and file.name.lower().endswith((".heic", ".heif")):
+            image = Image.open(file)
+            image = image.convert("RGB")
+
+            buffer = BytesIO()
+            image.save(buffer, format="JPEG", quality=90)
+            buffer.seek(0)
+
+            file = InMemoryUploadedFile(
+                buffer,
+                "ImageField",
+                file.name.rsplit(".", 1)[0] + ".jpg",
+                "image/jpeg",
+                buffer.getbuffer().nbytes,
+                None,
+            )
+
+        return file
+
+
 class GalleryItemForm(forms.ModelForm):
+
+    image = HEICImageField(required=False)
+    before_image = HEICImageField(required=False)
+    after_image = HEICImageField(required=False)
+
     class Meta:
         model = GalleryItem
         fields = "__all__"
