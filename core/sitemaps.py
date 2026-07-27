d1ff3d8a79a2d8eb31f9716af00844ceee1c7290
Yogendra Dhami <yogendradhami@Yogendras-MacBook-Air.local>
2026-07-28 08:54:05 +0930

    Add booking page to sitemap

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -25,7 +25,7 @@ class StaticViewSitemap(Sitemap):
     priority = 0.8
 
     def items(self):
-        return ["home", "contact"]
+        return ["home", "contact", "booking"]
 
     def location(self, item):
         return reverse(item)
