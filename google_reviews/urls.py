3be703fc932ba811f748a0b0d4c8601cf68e515c
Yogendra Dhami <yogendradhami@Yogendras-MacBook-Air.local>
2026-07-27 13:16:40 +0930

    Add Google review sync URL

--- google_reviews/urls.py
+++ google_reviews/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("google/reviews/", views.google_reviews, name="google_reviews"),
     path("google/test-calendar/", views.test_calendar_event, name="test_calendar_event"),
     path("google/customer-reviews/",views.get_google_reviews,name="get_google_reviews"),
-    path("google/sync-reviews/",views.sync_google_reviews,name="sync_google_reviews"),
-]
+    path("google/reviews/sync/", views.sync_google_reviews, name="sync_google_reviews"),
+
+    ]
