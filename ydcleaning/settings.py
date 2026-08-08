d5b74af9ec1350bc02793c86d31ef7d6f73e700f
Yogendra Dhami <yogendradhami@Yogendras-MacBook-Air.local>
2026-08-08 19:14:47 +0930

    Fix quote email timeout and error handling

--- ydcleaning/settings.py
+++ ydcleaning/settings.py
@@ -289,7 +289,7 @@ EMAIL_HOST = env.str("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = env.int("EMAIL_PORT", default=587)
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
 EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL", default=False)
-
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = env.str("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD", default="")
 
